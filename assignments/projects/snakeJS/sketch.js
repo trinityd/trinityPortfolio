8a6e56aaaed29183654f3fc88457698e9ef9fdc1
trinity <ftrinitydickinson@gmail.com>
2017-10-06 15:14:15 -0700

    First site commit

---
+++ assignments/projects/snakeJS/sketch.js
@@ -0,0 +1,56 @@
+var s;
+var scl = 10;
+var food;
+var score = "";
+
+function setup() {
+  createCanvas(400, 400);
+  frameRate(10);
+  s = new Snake();
+  addFood();
+}
+
+function draw() {
+  background(255);
+  noFill();
+  rect(0,0,width-1,height-1);
+  fill(0);
+  score = "Score: " + s.total;
+  text(score, 340, 20);
+  s.update();
+  s.show();
+  s.touchingBodyCheck();
+
+  if (s.eat(food)) {
+    addFood();
+  }
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+function mousePressed() {
+  s.total++;
+}
+
+function addFood() {
+  food = createVector(floor(random(width/scl))*scl, floor(random((height/scl)))*scl);
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    s.dir(0,-1);
+  }
+  else if (keyCode == DOWN_ARROW) {
+    s.dir(0,1);
+  }
+  else if (keyCode == RIGHT_ARROW) {
+    s.dir(1,0);
+  }
+  else if (keyCode == LEFT_ARROW) {
+    s.dir(-1,0);
+  }
+  else if (keyCode == SHIFT) {
+    addFood();
+  }
+}
---
+++ assignments/projects/snakeJS/snake.js
@@ -0,0 +1,56 @@
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.dir = function(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 2) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  this.touchingBodyCheck = function() {
+    for (var i = 0; i < this.tail.length; i++)
+    {
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1) {
+        this.total = 0;
+        this.tail = [];
+      }
+    }
+  }
+
+  this.update = function() {
+    for (var i = 0; i < this.tail.length-1; i++) {
+      this.tail[i] = this.tail[i+1];
+    }
+    this.tail[this.total-1] = createVector(this.x, this.y);
+
+    this.x = this.x + this.xspeed*scl;
+    this.y = this.y + this.yspeed*scl;
+
+    this.x = constrain(this.x, 0, width-scl-1);
+    this.y = constrain(this.y, 0, height-scl-1);
+  }
+
+  this.show = function() {
+    fill(0,255,0);
+    for (var i = 0; i < this.total; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+    rect(this.x, this.y, scl, scl);
+  }
+}
